05d6dcbd7275b417934029464c0c336d1360360d
Mian Mateen <mmuhammad8701gmail.com@Mians-MacBook-Air.local>
2025-07-01 22:20:31 -0400

    Reworked the todo.py application.

--- todo.py
+++ todo.py
@@ -1,33 +1,65 @@
 class ToDo:            # fix and test everything please i cannot do it right now because i am tired.
     def __init__(self):
         self.lists = []
+        self.add = None
+        self.counter = 0
+        try:
+            with open('text.txt', 'r') as f:
+                self.lists = f.readlines()
+            if self.counter == 0:
+                self.lists = [line.strip() for line in self.lists]
+            elif self.counter >= 1:
+                self.lists = [line + '/n' for line in self.lists]
+        except FileNotFoundError:
+            pass
     
     def todo_add(self):
-        self.add = input('> ')
-        with open('text.txt', 'r') as f:
-            lines = f.readlines()
-            if self.add in lines:
-                print(f"You have already have a task named {self.add}")
-                add_another = input('Are you sure you want to add another one? ').lower()
-                if add_another == 'yes' or add_another == 'y':
-                    self.lists.append(self.add + '\n')
-                    print(f"Another {self.add} Added!\n")
-                elif add_another == 'no' or add_another == 'n':
-                    print(f'{self.add} not added!\n')
-            elif self.add not in lines:
-                self.lists.append(self.add + '\n')
-                print(f'{self.add} Added!!\n')
+        addition = input('> ').strip().capitalize()
+        self.add = addition
+        try:
+            with open('text.txt', 'r') as f:
+                if self.counter == 0:
+                    self.lists = [line.strip() for line in self.lists]
+                elif self.counter >= 1:
+                    self.lists = [line + '/n' for line in self.lists]
+
+                if addition in self.lists:
+                    print(f"\nYou have already have a task named {addition}.")
+                    add_another = input('Add it anyway? (yes/no) ').lower()
+                    if add_another == 'yes' or add_another == 'y':
+                        self.lists.append(addition + '\n')
+                        print(f"Another {addition} Added!")
+                        self.counter += 1
+                    else:
+                        print(f'"{addition}" not added!\n')
+                        self.counter += 1
+                else:
+                    self.lists.append(addition + '\n')
+                    print(f'{addition} Added!!\n')
+        except FileNotFoundError:
+            with open('text.txt', 'a') as f:
+                f.write('New file created with the name text.txt')
+                print('Created a file that was non-exestant named: text.txt!!')
 
 
     def show_todo_list(self):
-        with open('text.txt', 'r') as f:
-            for task in f:
-                print(task, end=' ')
+        try:
+            with open('text.txt', 'r') as f:
+                tasks = [line.strip() for line in f if line.strip()]
+                
+                if not tasks:
+                    print('No tasks in the list!')
+                
+                print('\nYour todo list:')
+                for i, line in enumerate(tasks, start=1):
+                    print(f'{i}. {line}')
+        except FileNotFoundError:
+            print('No file task found, please add a task!')
 
 
     def save(self):
-        with open('text.txt', 'a') as f:
-            fContents = f.writelines(self.lists)
+        with open('text.txt', 'w') as f:
+            f.writelines(self.lists)
 
 
 while True:
@@ -36,12 +68,11 @@ while True:
 
     if command == 'add':
         todo.todo_add()
-    elif command in ['show todo', 'show', 'show todo list']:
+        todo.save()
+    elif command == 'show':
         todo.show_todo_list()
     elif command == 'help':
-        print("\nThe commands are Add, Remove, Save, or Show todo list(or just type show)\n")
-    elif command == 'save':
-        todo.save()
+        print("\nThe commands are Add, Remove, or Show todo list(or just type show)\n")
     elif command == 'quit':
         break
     else:
